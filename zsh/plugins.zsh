91dcece7a55fde4196ef861ec810614f344a5f3a
Filip Malinowski <crimhead@gmail.com>
2022-09-18 23:54:56 +0200

    replaced deprecated antigen with antidote

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,24 +1,4 @@
-# Use Antigen as zsh package manager
-source $(brew --prefix)/share/antigen/antigen.zsh
+# Use Antidote as zsh package manager
+source $(brew --prefix)/opt/antidote/share/antidote/antidote.zsh
 
-# Load the oh-my-zsh's library
-antigen use oh-my-zsh
-
-antigen bundles <<EOB
-  git
-  github
-  osx
-  ruby
-  brew
-  node
-  npm
-
-  zsh-users/zsh-syntax-highlighting
-  zsh-users/zsh-autosuggestions
-
-  b4b4r07/emoji-cli
-EOB
-
-antigen theme denysdovhan/spaceship-prompt spaceship
-
-antigen apply
+antidote load
